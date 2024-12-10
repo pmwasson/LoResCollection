77349d50dc15e558a4b63d2cf4af6e8dd48f25aa
pmwasson <pmwasson@gmail.com>
2024-12-10 15:03:57 -0800

    started robo

--- src/build.bat
+++ src/build.bat
@@ -14,6 +14,10 @@ python3 ..\scripts\imageConvert.py ..\images\land1.jpg  image.png >  ..\src\imag
 ca65 -I ..\src -t apple2 ..\src\lander.asm -l lander.dis || exit
 cl65 -I ..\src -t apple2 -u __EXEHDR__ ..\src\lander.asm apple2.lib  -o lander.apple2 -C ..\src\start2000.cfg || exit
 
+:: Robo
+ca65 -I ..\src -t apple2 ..\src\robo.asm -l robo.dis || exit
+cl65 -I ..\src -t apple2 -u __EXEHDR__ ..\src\robo.asm apple2.lib  -o robo.apple2 -C ..\src\start2000.cfg || exit
+
 :: Escape
 ca65 -I ..\src -t apple2 ..\src\escape.asm -l escape.dis || exit
 cl65 -I ..\src -t apple2 -u __EXEHDR__ ..\src\escape.asm apple2.lib  -o escape.apple2 -C ..\src\start2000.cfg || exit
@@ -29,10 +33,10 @@ cl65 -I ..\src -t apple2 -u __EXEHDR__ ..\src\landscape.asm apple2.lib  -o lands
 :: Start with a blank prodos disk
 copy ..\disk\template_prodos.dsk lores.dsk  || exit
 
-
 java -jar C:\jar\AppleCommander.jar -as lores.dsk lander.system sys < lander.apple2  || exit
 java -jar C:\jar\AppleCommander.jar -as lores.dsk land.system sys < landscape.apple2  || exit
 java -jar C:\jar\AppleCommander.jar -as lores.dsk escape.system sys < escape.apple2  || exit
+java -jar C:\jar\AppleCommander.jar -as lores.dsk robo.system sys < robo.apple2  || exit
 
 :: Throw on basic
 java -jar C:\jar\AppleCommander.jar -p   lores.dsk basic.system sys < ..\disk\BASIC.SYSTEM  || exit
---
+++ src/robo.asm
@@ -0,0 +1,234 @@
+;-----------------------------------------------------------------------------
+; Paul Wasson - 2024
+;-----------------------------------------------------------------------------
+; Robo
+;-----------------------------------------------------------------------------
+; Low res shooter
+;-----------------------------------------------------------------------------
+
+.include "defines.asm"
+.include "macros.asm"
+
+.segment "CODE"
+.org    $2000
+
+.proc main
+
+    ;----------------------------------
+    ; Init
+    ;----------------------------------
+    jsr         HOME        ; clear screen
+    jsr         GR          ; set low-res graphics mode
+    sta         MIXCLR      ; full screen
+    bit         HISCR       ; Display page2 so switch to page1
+
+loop:
+    ; flip page
+    lda         PAGE2
+    bmi         :+
+    bit         HISCR
+    jmp         clear
+:
+    bit         LOWSCR
+
+clear:
+    ; clear screen
+    jsr         clearScreenWithEffect
+
+    jsr         readJoystick
+    jsr         updateEffect
+
+    jmp         loop
+
+.endproc
+
+
+.proc updateEffect
+    ; clear previous
+    lda         #0
+    ldx         prevRow
+    sta         rowColor,x
+    ldx         prevCol
+    sta         colColor,x
+    sta         colColor+40,x
+    sta         colColor+80,x
+
+    ldx         paddleX
+    lda         paddleColTable,x
+    sta         prevCol
+    tax
+    lda         #$ff
+    sta         colColor,x
+    sta         colColor+40,x
+    sta         colColor+80,x
+
+    ldx         paddleY
+    lda         paddleRowTable,x
+    lsr
+    sta         prevRow
+    tax
+    lda         #$0f
+    bcc         :+
+    lda         #$f0
+:
+    sta         rowColor,x
+    rts
+
+prevRow:        .byte   0
+prevCol:        .byte   0
+
+.align 256
+
+paddleRowTable:
+    .byte        0, 0, 0, 0, 0, 0                                                       ; 6
+    .byte        0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9,10,10,11,11 ; 24
+    .byte       12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20,21,21,22,22,23,23 ; 24
+    .byte       24,24,25,25,26,26,27,27,28,28,29,29,30,30,31,31,32,32,33,33,34,34,35,35 ; 24
+    .byte       36,36,37,37,38,38,39,39,40,40,41,41,42,42,43,43,44,44,45,45,46,46,47,47 ; 24
+    .byte       47,47,47,47,47,47                                                       ; 6  = 108
+
+paddleColTable:
+    .byte        0, 0, 0, 0                                                                     ; 4
+    .byte        0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9, 9      ; 25
+    .byte       10,10,11,11,11,12,12,13,13,13,14,14,15,15,15,16,16,17,17,17,18,18,19,19,19      ; 25
+    .byte       20,20,21,21,21,22,22,23,23,23,24,24,25,25,25,26,26,27,27,27,28,28,29,29,29      ; 25
+    .byte       30,30,31,31,31,32,32,33,33,33,34,34,35,35,35,36,36,37,37,37,38,38,39,39,39      ; 25
+    .byte       39,39,39,39                                                                     ; 4  = 108
+.endproc
+
+;-----------------------------------------------------------------------------
+; Clear Screen (with effect)
+;   Clear low res screen with single horizontal (page0) or vertical (page1)
+;   line.
+;-----------------------------------------------------------------------------
+
+.proc clearScreenWithEffect
+
+    lda         PAGE2           ; bit 7 = page2 displayed
+    bmi         clear0          ; display high, so draw low
+    jmp         clear1
+
+clear0:
+    ldx         #39
+loop0:
+    lda         rowColor+0
+    sta         $0400,x
+    lda         rowColor+1
+    sta         $0480,x
+    lda         rowColor+2
+    sta         $0500,x
+    lda         rowColor+3
+    sta         $0580,x
+    lda         rowColor+4
+    sta         $0600,x
+    lda         rowColor+5
+    sta         $0680,x
+    lda         rowColor+6
+    sta         $0700,x
+    lda         rowColor+7
+    sta         $0780,x
+    lda         rowColor+8
+    sta         $0428,x
+    lda         rowColor+9
+    sta         $04A8,x
+    lda         rowColor+10
+    sta         $0528,x
+    lda         rowColor+11
+    sta         $05A8,x
+    lda         rowColor+12
+    sta         $0628,x
+    lda         rowColor+13
+    sta         $06A8,x
+    lda         rowColor+14
+    sta         $0728,x
+    lda         rowColor+15
+    sta         $07A8,x
+    lda         rowColor+16
+    sta         $0450,x
+    lda         rowColor+17
+    sta         $04D0,x
+    lda         rowColor+18
+    sta         $0550,x
+    lda         rowColor+19
+    sta         $05D0,x
+    lda         rowColor+20
+    sta         $0650,x
+    lda         rowColor+21
+    sta         $06D0,x
+    lda         rowColor+22
+    sta         $0750,x
+    lda         rowColor+23
+    sta         $07D0,x
+    dex
+    bmi         :+
+    jmp         loop0
+:
+    rts
+
+clear1:
+    ldx         #40*3
+loop1:
+    lda         colColor,x
+    sta         $800,x
+    sta         $880,x
+    sta         $900,x
+    sta         $980,x
+    sta         $A00,x
+    sta         $A80,x
+    sta         $B00,x
+    sta         $B80,x
+    dex
+    bpl         loop1
+    rts
+.endproc
+
+.align 32
+rowColor:       .res    24
+
+.align 128
+colColor:       .res    40*3
+
+
+
+;-----------------------------------------------------------------------------
+; Read Joystick
+;   Read both paddles in a constant time routine
+;   (Not faster if value is lower, which messes up game loop timing)
+;   Range is $00 .. $6B (107) since sampled less often than PREAD
+;   Range measured on AppleWin, but assumed similar to real HW.
+;-----------------------------------------------------------------------------
+PADDLE_MAX      = $6B
+.align 64               ; For timing, make sure branches don't cross pages
+
+.proc readJoystick
+
+    lda         #0
+    sta         paddleX
+    sta         paddleY
+    sta         GCRESET
+    ldx         #PADDLE_MAX
+
+loop:
+    lda         GC0
+    bpl         done0
+    inc         paddleX
+read1:
+    lda         GC1         ; 4 cycles
+    bpl         done1       ; 2 cycles (+1 for taken)
+    inc         paddleY     ; 6 cycle
+finish:
+    dex
+    bne         loop
+    rts
+
+done0:                      ; +1 cycle for taken branch
+    nop                     ; +2
+    jmp         read1       ; +3 = 6 (same as inc above)
+done1:
+    nop
+    jmp         finish
+
+.endproc
+
+paddleX:        .byte       0
+paddleY:        .byte       0
